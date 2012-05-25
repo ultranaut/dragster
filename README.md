4c358399e901439b83fdd5507171ce1181f0cca2
John Shearer <jshearer@wevelgem.local>
2012-05-25 12:18:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+dragster.js
+===========
+
+For when you want to drag some stuff around; when you get tired of that, drop it.
---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8"/>
+  <title>Dragster</title>
+  <link rel="stylesheet" href="css/dragster.css"/>
+</head>
+<body>
+<div id="container">
+
+  <div id="blank" class="target unit size2of3"></div>
+
+  <div class="modules lastUnit size1of3">
+    <div id="spot" class="unit size1of2">
+      <article class="mod spot">
+        Spot
+      </article>
+    </div>
+
+    <div id="recipe" class="unit size1of2">
+      <article class="mod recipe">
+        Recipe
+      </article>
+    </div>
+  </div>
+
+</div>
+<script src="js/dragster.js"></script>
+</body>
+</html>
